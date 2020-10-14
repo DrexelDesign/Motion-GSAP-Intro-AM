482db0c3e214de9ea9f46d0c71d9614e32d9b577
Josh <josh@gdovindesigns.com>
2020-10-14 10:49:24 -0400

    mask demo complete

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,9 +1,13 @@
 import {gsap} from "gsap";
 
+gsap.set("#mask",{transformOrigin: "center"});
+
 const iceCreamTL = gsap.timeline();
-iceCreamTL.from("#ice-cream-cp",{duration:3, y:-200,ease: "bounce.out"}, "iceCreamGo")
-        .from("#ice-cream",{duration:3, y:400, ease:"none"}, "iceCreamGo")
-        .to("#ice-cream",{duration:3, y:-400, ease:"none"},"iceCreamGo");
+iceCreamTL.to("#mask",{duration:3, scale:4}, "iceCreamGo")
+            .from("#ice-cream",{duration:3, y:400, ease:"none"}, "iceCreamGo")
+            .to("#ice-cream",{duration:3, y:-400, ease:"none"},"iceCreamGo");
+
+        
 
 export function iceCreamAnimation(){
     return iceCreamTL;
