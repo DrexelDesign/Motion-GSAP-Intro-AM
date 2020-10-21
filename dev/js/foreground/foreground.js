9ea7e0b014abb0b955d18ecb3ff857e6065e2faf
Josh <josh@gdovindesigns.com>
2020-10-21 11:43:22 -0400

    updated demo

---
+++ dev/js/foreground/foreground.js
@@ -0,0 +1,13 @@
+const { default: gsap } = require("gsap/gsap-core");
+
+import {gsap} from "gsap";
+
+const foregroundTL = gsap.timeline();
+
+export function foregroundAnimation(){
+
+    foregroundTL.from("#back",{duration: 3, scale:6, y:1000})
+                .from("#middle",{duration: 2, scale:6, y:1000},"-=1.5");
+
+    return foregroundTL;
+}
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,13 +1,15 @@
-import { gsap } from "gsap";
-import { GSDevTools } from "gsap/GSDevTools";
+import {gsap} from "gsap";
+import {GSDevTools} from "gsap/GSDevTools";
 
 gsap.registerPlugin(GSDevTools);
 
 import {backgroundAnimation} from "./background/background.js";
+import {foregroundAnimation} from "./foreground/foreground.js";
 
 const mainTL = gsap.timeline();
 
-mainTL.add(backgroundAnimation());
+mainTL.add(backgroundAnimation())
+      .add(foregroundAnimation());
 
 
 //instantiate GSDevTools with default settings
