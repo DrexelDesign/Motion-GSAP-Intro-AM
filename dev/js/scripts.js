eba60bb9d782f40c9f1bbd5a0c24a2a8b6827d1d
Josh <josh@gdovindesigns.com>
2020-11-02 10:10:33 -0500

    completed demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,14 +1,14 @@
 import gsap from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
-//import { flamesAnimation } from "./rocket.js";
+import { flamesAnimation } from "./rocket.js";
 import { rocketAnimation } from "./rocket.js";
 
 gsap.registerPlugin(GSDevTools);
 
-//flamesAnimation();
+flamesAnimation();
 
 const mainTL = gsap.timeline();
 mainTL.add(rocketAnimation());
 
 //instantiate GSDevTools with default settings
-GSDevTools.create();
+//GSDevTools.create();
