9893f5450902c20306751746d171e5524d1c4177
Josh <josh@gdovindesigns.com>
2020-11-09 07:18:11 -0500

    animated all trees

--- dev/js/foreground/foreground.js
+++ dev/js/foreground/foreground.js
@@ -12,8 +12,14 @@ export function foregroundAnimation(){
                 .from("#grass",{duration: 2, scale:6, y:1000},"-=2")
                 
                 // trees
-                .from("#tree-back-left-cp",{duration:1, scale:0, alpha:0},"-=2")
-                .from("#tree-front-right-cp",{duration:1, scale:0, alpha:0})
+                .from("#tree-back-right-cp",{duration:2, alpha:0, scale:0},"-=2")
+                .from("#tree-back-left-cp",{duration:2, scale:0, alpha:0},"-=2")
+
+                .from("#tree-middle-right-cp",{duration:2, alpha:0, scale:0},"-=1.5")
+                .from("#tree-middle-left-cp",{duration:2, alpha:0, scale:0},"-=1.5")
+
+                .from("#tree-front-right-cp",{duration:2, scale:0, alpha:0},"-=2")
+                .from("#tree-front-left-cp",{duration:2, alpha:0, scale:0},"-=2")
                 
                 // crosses
                 .from("#cross-left",{duration:0.25, scaleY:0},"-=1.5")
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,7 +4,7 @@ import {GSDevTools} from "gsap/GSDevTools";
 import {backgroundAnimation} from "./background/background.js";
 import {foregroundAnimation} from "./foreground/foreground.js";
 import {skyAnimation} from "./background/sky.js";
-import {catAnimation} from "./foreground/cat.js";
+//import {catAnimation} from "./foreground/cat.js";
 //import {cloudAnimation} from "./background/sky.js";
 
 import {batFourAnimation} from "./foreground/bat.js";
@@ -21,8 +21,7 @@ const mainTL = gsap.timeline();
 
 mainTL.add(backgroundAnimation(),"startSpooky")
       .add(skyAnimation(),"startSpooky")
-      .add(foregroundAnimation(),"startSpooky")
-      .add(catAnimation());
+      .add(foregroundAnimation(),"startSpooky");
 
 //instantiate GSDevTools with default settings
 GSDevTools.create();
