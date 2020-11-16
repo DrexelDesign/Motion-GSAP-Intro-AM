18940b89e9e8ba4c73ae8a1cedf9416123372dc2
Josh <josh@gdovindesigns.com>
2020-11-16 11:50:16 -0500

    morph demo completed

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,32 +1,34 @@
 import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
+
+MorphSVGPlugin.convertToPath("circle, rect, ellipse, line, polygon, polyline");
 
 
-const drawTL = gsap.timeline();
+// const morphTL = gsap.timeline();
+
+gsap.set("#light-red-1",{alpha:0});
+
+const monsterTL = gsap.timeline();
 
 
 export function drawAnimation(){
 
-    //drawTL.from("#box",{duration:2, drawSVG: "0%", ease:"none"});
+    // morphTL.to("#blue",{duration:1, morphSVG:"#star", ease: "bounce.out"});
+    // return morphTL;
+    monsterTL.to("#shadow-1",{duration:1, morphSVG:"#shadow-2"})
+    monsterTL.to("#dark-red-1",{duration:1, morphSVG:"#dark-red-2", fill:"#000"},"monster")
+    monsterTL.to("#light-red-1",{duration:1, morphSVG:"#light-red-2", alpha:1},"monster")
+    monsterTL.from("#light-red-1",{duration:1, drawSVG:"0%"},"monster")
+    monsterTL.to("#right-eye-1",{duration:1, morphSVG:"#right-eye-2", fill:"#fff"},"monster")
+    monsterTL.to("#left-eye-1",{duration:1, morphSVG:"#left-eye-2"},"monster")
+    monsterTL.to("#mouth-1",{duration:1, morphSVG:"#mouth-2"},"monster")
+    monsterTL.to("#leg-1",{duration:1, morphSVG:"#horn-2"},"monster")
+    monsterTL.to("#leg-2",{duration:1, morphSVG:"#horn-2"},"monster");
 
-    //drawTL.to("#box",{duration:2, drawSVG:"0%"});
-
-     //drawTL.to("#box",{duration:2, drawSVG:"20% 60%"});
-
-    //drawTL.to("#box",{duration:2, drawSVG:0});
-
-    //drawTL.to("#box",{duration:2, drawSVG:"40% 30%"});
-
-    //drawTL.to("#box",{duration:2, drawSVG:"-10% -90%"});
-
-    //drawTL.from("#box",{duration:2, drawSVG:"50% 50%"});
-
-    //drawTL.from("#box",{duration:2, drawSVG:"10% 50%"});
-
-    drawTL.fromTo("#box",{drawSVG:"0% 10%"},{duration:2, drawSVG: "90% 100%"})
-          .to("#box",{duration:2, drawSVG: "100%"});
-
-    return drawTL;
+    // does not work
+    // monsterTL.to("#monster-1",{duration:1, morphSVG:"#monster-2"})
+     return monsterTL;
 }
